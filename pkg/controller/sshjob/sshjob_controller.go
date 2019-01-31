909a142be718897a6ef9903d6f4f2a6f578eb11a
Charlie Getzen <charliegetzenlc@gmail.com>
2019-01-30 22:00:09 -0800

    refactor to remove destroy job being triggered when it doesn't exist (#12)
    * refactor to remove destroy job being triggered when it doesn't exist
    
    * remove comments

--- pkg/controller/sshjob/sshjob_controller.go
+++ pkg/controller/sshjob/sshjob_controller.go
@@ -84,12 +84,10 @@ type ReconcileSSHJob struct {
 // Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
 func (r *ReconcileSSHJob) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
-	reqLogger.Info("Reconciling SSHJob")
 
 	// Fetch the SSHJob instance
 	instance := &corev1.Pod{}
-	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
-	if err != nil {
+	if err := r.client.Get(context.TODO(), request.NamespacedName, instance); err != nil {
 		if errors.IsNotFound(err) {
 			// Request object not found, could have been deleted after reconcile request.
 			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
@@ -99,7 +97,6 @@ func (r *ReconcileSSHJob) Reconcile(request reconcile.Request) (reconcile.Result
 		// Error reading the object - requeue the request.
 		return reconcile.Result{}, err
 	}
-
 
   annotationIn := instance.ObjectMeta.Annotations["ssh.in"]
   annotationOut := instance.ObjectMeta.Annotations["ssh.out"]
@@ -112,57 +109,25 @@ func (r *ReconcileSSHJob) Reconcile(request reconcile.Request) (reconcile.Result
 		return reconcile.Result{}, err
 	}
 
-	// Check if this Pod already exists
-	found := &corev1.Pod{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, found)
+  found := &corev1.Pod{}
+  err := r.client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, found)
 
-  if len(annotationIn) > 0 {
-  	if err != nil && errors.IsNotFound(err) {
-  		reqLogger.Info("Creating a new Pod", "Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
-  		err = r.client.Create(context.TODO(), pod)
-  		if err != nil {
-  			return reconcile.Result{}, err
-  		}
-      // MIGHT need to put logic here or return an error - TEST
+  podShouldExist := len(annotationIn) > 0
+  podExists := err == nil || !errors.IsNotFound(err)
 
-  		// Pod created successfully - don't requeue
-  		return reconcile.Result{}, nil
-  	} else if err != nil {
-  		return reconcile.Result{}, err
-  	}
-
-    // Move this logic to container
-    // if len(annotationOut) == 0 {
-    //   fmt.Println("Here")
-    //   annotationsWorker := found.ObjectMeta.Annotations["ssh"]
-    //   fmt.Println(annotationsWorker)
-    //   if len(annotationsWorker) > 0 {
-    //     // copy the annotation
-    //     m := make(map[string]string)
-    //     currentAnnotations := instance.GetAnnotations()
-    //     for k, v := range currentAnnotations {
-    //       m[k] = v
-    //     }
-    //     m["ssh.out"] = annotationsWorker
-    //
-    //     instance.SetAnnotations(m)
-    //
-    //     err = r.client.Update(context.Background(), instance)
-    //     if err != nil {
-    //       return reconcile.Result{}, nil
-    //     }
-    //   	reqLogger.Info("Adding annotation", "Pod.Namespace", instance.Namespace, "Pod.Name", instance.Name)
-    //   	return reconcile.Result{}, nil
-    //    } else {
-    //      // CREATE NEW Error here
-    //   }
-    // }
-
-    // DELETE --v 2 lines
-  	// Pod already exists - don't requeue
+  switch {
+  case podExists && podShouldExist:
   	reqLogger.Info("Skip reconcile: Pod already exists", "Pod.Namespace", found.Namespace, "Pod.Name", found.Name)
-  	return reconcile.Result{}, nil
-  } else {
+    return reconcile.Result{}, nil
+  case !podExists && podShouldExist:
+		reqLogger.Info("Creating a new Pod", "Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
+		if err = r.client.Create(context.TODO(), pod); err != nil {
+			return reconcile.Result{}, err
+		}
+		// Pod created successfully - don't requeue
+		return reconcile.Result{}, nil
+  case podExists && !podShouldExist:
+    // Clean up annotations
     if len(annotationOut) > 0 {
       reqLogger.Info("Remove ssh annotation", "Pod.Namespace", instance.Namespace, "Pod.Name", instance.Name)
       m := make(map[string]string)
@@ -173,24 +138,18 @@ func (r *ReconcileSSHJob) Reconcile(request reconcile.Request) (reconcile.Result
         }
       }
       instance.SetAnnotations(m)
-      err = r.client.Update(context.Background(), instance)
-      if err != nil {
-        return reconcile.Result{}, nil
+      if err := r.client.Update(context.Background(), instance); err != nil {
+        return reconcile.Result{}, err
       }
     }
-    // Dont forget to strip out labels
-    if err != nil && errors.IsNotFound(err) {
-      return reconcile.Result{}, nil
-    } else if err != nil {
-      return reconcile.Result{}, err
-    }
-    reqLogger.Info("Deleting a Pod", "Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name, "Annotation", annotationIn)
-    err = r.client.Delete(context.TODO(), pod)
-    if err != nil {
+    // Delete pod
+    reqLogger.Info("Deleting a Pod", "Pod.Namespace", found.Namespace, "Pod.Name", found.Name, "Annotation", annotationIn)
+    if err := r.client.Delete(context.TODO(), found); err != nil {
       return reconcile.Result{}, err
     }
     return reconcile.Result{}, nil
   }
+  return reconcile.Result{}, nil
 }
 
 // newPodForCR returns a busybox pod with the same name/namespace as the cr
